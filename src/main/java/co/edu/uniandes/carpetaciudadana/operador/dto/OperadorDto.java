8c13f0138132d7a1439d9e69b6818656829de364
pedrazadumer <pedrazadumer@gmail.com>
2021-05-26 23:54:15 -0500

    Creando la estructura inicial y cargue inicial del contexto con 1 endpoint.

---
+++ src/main/java/co/edu/uniandes/carpetaciudadana/operador/dto/OperadorDto.java
@@ -0,0 +1,11 @@
+package co.edu.uniandes.carpetaciudadana.operador.dto;
+
+import lombok.Data;
+
+@Data
+public class OperadorDto {
+
+    private String nombre;
+    private String endpoint;
+
+}
---
+++ src/main/java/co/edu/uniandes/carpetaciudadana/operador/dto/RespuestaEntidadPublicaDto.java
@@ -0,0 +1,10 @@
+package co.edu.uniandes.carpetaciudadana.operador.dto;
+
+import lombok.Data;
+
+@Data
+public class RespuestaEntidadPublicaDto {
+
+    private String correoEntidad;
+
+}
