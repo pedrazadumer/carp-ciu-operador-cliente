c688f8b709fa311a0fb6273d0f1c881efdd6c23d
pedrazadumer <pedrazadumer@gmail.com>
2021-05-31 23:48:20 -0500

    Aceptando plano como arreglo.

--- src/main/java/co/edu/uniandes/carpetaciudadana/operador/dto/PeticionEnviarPaquete.java
+++ src/main/java/co/edu/uniandes/carpetaciudadana/operador/dto/PeticionEnviarPaquete.java
@@ -1,10 +1,12 @@
 package co.edu.uniandes.carpetaciudadana.operador.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 @Data
 public class PeticionEnviarPaquete {
     private String nit;
     private String urlEntidad;
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
     private String[] archivos;
 }
