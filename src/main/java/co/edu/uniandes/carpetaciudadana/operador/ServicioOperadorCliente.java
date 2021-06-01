fdfd5228a7f30a44239af0cc1ab78ba62d380262
pedrazadumer <pedrazadumer@gmail.com>
2021-05-31 22:49:43 -0500

    Agregando nuevos parametros envio archivos.

--- src/main/java/co/edu/uniandes/carpetaciudadana/operador/ServicioOperadorCliente.java
+++ src/main/java/co/edu/uniandes/carpetaciudadana/operador/ServicioOperadorCliente.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServicioOperador extends SpringBootServletInitializer {
+public class ServicioOperadorCliente extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(ServicioOperador.class, args);
+        SpringApplication.run(ServicioOperadorCliente.class, args);
     }
 }
